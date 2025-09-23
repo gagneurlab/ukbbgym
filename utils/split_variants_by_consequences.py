9e976ec2f7d74c0737e6985b06781e9e65819b14
ShubhankarLondhe <shubhankar.londhe@gmail.com>
2025-09-23 20:29:38 +0000

    fixed bugs

--- utils/split_variants_by_consequences.py
+++ utils/split_variants_by_consequences.py
@@ -3,49 +3,49 @@ import polars as pl
 
 # define consequence groups
 variant_groups = {
-    "intronic": ['Consequence_intron_variant'],
-    "missense": ['Consequence_missense_variant'],
+    "intronic": ['consequence_intron_variant'],
+    "missense": ['consequence_missense_variant'],
     "plof": [
-        'Consequence_frameshift_variant',
-        'Consequence_start_lost',
-        'Consequence_stop_gained',
-        'Consequence_stop_lost',
-        'Consequence_splice_donor_variant',
-        'Consequence_splice_acceptor_variant'
+        'consequence_frameshift_variant',
+        'consequence_start_lost',
+        'consequence_stop_gained',
+        'consequence_stop_lost',
+        'consequence_splice_donor_variant',
+        'consequence_splice_acceptor_variant'
     ],
     # Splice variants also in plof and other_low_impact. All other groups are mutually exclusive and cover all variants
     "splicing": [
-        'Consequence_splice_donor_variant',
-        'Consequence_splice_acceptor_variant',
-        'Consequence_splice_donor_5th_base_variant',
-        'Consequence_splice_donor_region_variant',
-        'Consequence_splice_polypyrimidine_tract_variant',
-        'Consequence_splice_region_variant'
+        'consequence_splice_donor_variant',
+        'consequence_splice_acceptor_variant',
+        'consequence_splice_donor_5th_base_variant',
+        'consequence_splice_donor_region_variant',
+        'consequence_splice_polypyrimidine_tract_variant',
+        'consequence_splice_region_variant'
     ],
     "utr": [
-        'Consequence_3_prime_UTR_variant',
-        'Consequence_5_prime_UTR_variant'
+        'consequence_3_prime_utr_variant',
+        'consequence_5_prime_utr_variant'
     ],
     "upstream_downstream": [
-        'Consequence_upstream_gene_variant',
-        'Consequence_downstream_gene_variant'
+        'consequence_upstream_gene_variant',
+        'consequence_downstream_gene_variant'
     ],
     "other_low_impact": [
-        'Consequence_splice_donor_5th_base_variant',
-        'Consequence_splice_donor_region_variant',
-        'Consequence_splice_polypyrimidine_tract_variant',
-        'Consequence_splice_region_variant',
-        'Consequence_inframe_deletion',
-        'Consequence_inframe_insertion',
-        'Consequence_intergenic_variant',
-        'Consequence_non_coding_transcript_exon_variant',
-        'Consequence_non_coding_transcript_variant',
-        'Consequence_protein_altering_variant',
-        'Consequence_start_retained_variant',
-        'Consequence_stop_retained_variant',
-        'Consequence_synonymous_variant',
-        'Consequence_coding_sequence_variant',
-        'Consequence_NMD_transcript_variant'
+        'consequence_splice_donor_5th_base_variant',
+        'consequence_splice_donor_region_variant',
+        'consequence_splice_polypyrimidine_tract_variant',
+        'consequence_splice_region_variant',
+        'consequence_inframe_deletion',
+        'consequence_inframe_insertion',
+        'consequence_intergenic_variant',
+        'consequence_non_coding_transcript_exon_variant',
+        'consequence_non_coding_transcript_variant',
+        'consequence_protein_altering_variant',
+        'consequence_start_retained_variant',
+        'consequence_stop_retained_variant',
+        'consequence_synonymous_variant',
+        'consequence_coding_sequence_variant',
+        'consequence_NMD_transcript_variant'
     ]
 }
 
@@ -53,7 +53,8 @@ out_dir = "/home/dnanexus/data_dir/variant_classes"
 os.makedirs(out_dir, exist_ok=True)
 
 # available columns in the anno LazyFrame
-anno = pl.scan_parquet("/home/dnanexus/data_dir/cadd_vep_annotations_processed_final_selected.parquet")
+# anno = pl.scan_parquet("/home/dnanexus/data_dir/cadd_vep_annotations_processed_final_selected.parquet")
+anno = pl.scan_parquet("/home/dnanexus/data_dir/genebass1e6_genes_10kb_allvars_annotated_250923.parquet")
 available_cols = anno.collect_schema().names()
 
 for grp_name, cols in variant_groups.items():
@@ -73,20 +74,20 @@ for grp_name, cols in variant_groups.items():
     select_cols = ['id', 'region'] + cols_present
     lf = anno.filter(mask)#.select([c for c in select_cols if c in available_cols])
 
-    out_path = os.path.join(out_dir, f"genebass1e6_{grp_name}_variants.parquet")
+    out_path = os.path.join(out_dir, f"genebass1e6_genes_10kb_{grp_name}_variants_annotated_250923.parquet")
 
     # schedule sink and trigger execution with streaming engine
     lf.sink_parquet(out_path, engine='streaming')
 
-
 # After writing group-specific files, also write variants with NONE of the consequence columns == 1
+cons_annos = sorted({col for cols_list in variant_groups.values() for col in cols_list})
 cols_cons_present = [c for c in cons_annos if c in available_cols]
 mask_none = None
 for c in cols_cons_present:
     expr = (pl.col(c) == 1)
     mask_none = (~expr) if mask_none is None else (mask_none & ~expr)
 
-out_path_none = os.path.join(out_dir, "genebass1e6_NOconsequence_variants.parquet")
+out_path_none = os.path.join(out_dir, "genebass1e6_NOconsequence_variants_250923.parquet")
 lf_none = anno.filter(mask_none)#.select([c for c in ['id', 'region'] + cols_cons_present if c in available_cols])
 lf_none.sink_parquet(out_path_none, engine='streaming')
 print(f"Wrote unannotated variants -> {out_path_none}")
